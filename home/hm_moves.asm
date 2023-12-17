52fa455cad143d3635f99a840ffd33eaa89f4ddb
itnevres <serventi.alex@gmail.com>
2023-12-17 00:21:49 -0800

    Make TMs reusable, Make HMs forgettable, cheapen Game Corner rewards, cheapen Game Corner coin costs (/100) increase start $ (100x) and Mom Money (10x)

--- home/hm_moves.asm
+++ home/hm_moves.asm
@@ -15,11 +15,12 @@ IsHMMove::
 	jp IsInArray
 
 .HMMoves:
-	db CUT
-	db FLY
-	db SURF
-	db STRENGTH
-	db FLASH
-	db WATERFALL
-	db WHIRLPOOL
+; Hopefully commenting these out makes HM moves forgettable.
+;	db CUT
+;	db FLY
+;	db SURF
+;	db STRENGTH
+;	db FLASH
+;	db WATERFALL
+;	db WHIRLPOOL
 	db -1 ; end
